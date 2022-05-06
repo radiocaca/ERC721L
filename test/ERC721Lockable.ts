816f645dc18dbe58c2626ab1890d617e7bc9a845
gojazdev <103745011+gojazdev@users.noreply.github.com>
2022-05-06 09:09:19 +0800

    feat(contracts): note bugs in _beforeTokenLock

--- test/ERC721Lockable.ts
+++ test/ERC721Lockable.ts
@@ -29,18 +29,16 @@ describe("ERC721Lockable contract", function () {
 
   it("lockMint works", async function () {
     const NFTId = 0;
-    const block = await ethers.provider.getBlock("latest");
-    const expired = block.number + 1;
-    await EIP5058.lockMint(owner.address, NFTId, expired);
+    const block = await ethers.provider.getBlockNumber()
+    await EIP5058.lockMint(owner.address, NFTId, block + 2);
 
     expect(await EIP5058.isLocked(NFTId)).eq(true);
   });
 
   it("Can not transfer when token is locked", async function() {
     const NFTId = 0;
-    const block = await ethers.provider.getBlock("latest");
-    const expired = block.number + 1;
-    await EIP5058.lockMint(owner.address, NFTId, expired);
+    const block = await ethers.provider.getBlockNumber()
+    await EIP5058.lockMint(owner.address, NFTId, block + 3);
 
     // can not transfer when token is locked
     await expect(EIP5058.transferFrom(owner.address, alice.address, NFTId)).to.be.revertedWith(
@@ -48,17 +46,38 @@ describe("ERC721Lockable contract", function () {
     );
   });
 
-  it("Can transfer when token is expired", async function() {
+  it("isLocked works", async function() {
     const NFTId = 0;
-    const block = await ethers.provider.getBlock("latest");
-    const expired = block.number + 1;
-    await EIP5058.lockMint(owner.address, NFTId, expired);
+    const block = await ethers.provider.getBlockNumber()
+    await EIP5058.lockMint(owner.address, NFTId, block + 2);
 
-    // can transfer when lock is expired
-    await ethers.provider.send('evm_mine', []);
-    await expect(EIP5058.transferFrom(owner.address, alice.address, NFTId)).to.be.revertedWith(
-      "ERC721L: token transfer while locked",
-    );
+    // isLocked works
+    expect(await EIP5058.isLocked(NFTId)).eq(true);
+    await ethers.provider.send("evm_mine", []);
+    expect(await EIP5058.isLocked(NFTId)).eq(false);
+  });
+
+  // it("lockFrom works", async function() {
+  //   const NFTId = 0;
+  //   let block = await ethers.provider.getBlockNumber()
+  //   await EIP5058.lockMint(owner.address, NFTId, block + 2);
+  // 
+  //   // lockFrom works
+  //   // await ethers.provider.send("evm_mine", []);
+  //   // block = await ethers.provider.getBlockNumber()
+  //   await EIP5058.lockFrom(owner.address, NFTId, block + 4);
+  // });
+
+  it("unlockFrom works", async function() {
+    const NFTId = 0;
+    const block = await ethers.provider.getBlockNumber()
+    await EIP5058.lockMint(owner.address, NFTId, block + 3);
+
+    // unlock works
+    expect(await EIP5058.isLocked(NFTId)).eq(true);
+    expect(await EIP5058.lockerOf(NFTId)).eq(owner.address);
+    await EIP5058.unlockFrom(owner.address, NFTId);
+    expect(await EIP5058.isLocked(NFTId)).eq(false);
   });
 
   it("lockApprove works", async function () {
@@ -67,5 +86,21 @@ describe("ERC721Lockable contract", function () {
     await EIP5058.lockApprove(alice.address, NFTId);
 
     expect(await EIP5058.getLockApproved(NFTId)).eq(alice.address);
+  });
+
+  // it("Can lock while lockApproved", async function() {
+  //   const NFTId = 0;
+  //   const block = await ethers.provider.getBlockNumber()
+  //   await EIP5058.lockMint(owner.address, NFTId, block + 2);
+  //   await EIP5058.lockApprove(alice.address, NFTId);
+  //
+  // });
+
+  it("setLockApproveForAll works", async function() {
+    await EIP5058.setLockApprovalForAll(alice.address, true);
+    expect(await EIP5058.isLockApprovedForAll(owner.address, alice.address)).eq(true);
+
+    await EIP5058.setLockApprovalForAll(alice.address, false);
+    expect(await EIP5058.isLockApprovedForAll(owner.address, alice.address)).eq(false);
   });
 });
