c0f6ee3c2246ce021f9651661b7272e1909ffb96
vincent <vincent@test.com>
2022-05-11 14:45:33 +0800

    fix mintBatch, optimize mintRange

--- contracts/presets/ERC721Presets.sol
+++ contracts/presets/ERC721Presets.sol
@@ -70,7 +70,7 @@ contract ERC721Presets is
     }
 
     function mintBatch(address to, uint256[] calldata tokenIds) external onlyRole(MINTER_ROLE) {
-        for (uint256 i = 0; i <= tokenIds.length; i++) {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
             _mint(to, tokenIds[i]);
         }
     }
@@ -80,8 +80,8 @@ contract ERC721Presets is
         uint256 fromId,
         uint256 toId
     ) external onlyRole(MINTER_ROLE) {
-        for (uint256 i = fromId; i <= toId; i++) {
-            _mint(to, i);
+        for (; fromId <= toId; fromId++) {
+            _mint(to, fromId);
         }
     }
 
